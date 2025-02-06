e395b1983f6cfb810bd61c4a3e03fa596616f199
unknown <jagotabrahm@gmail.com>
2025-02-06 14:07:32 +0530

    f

--- app/api/todo/[taskId]/status/route.ts
+++ app/api/todo/[taskId]/status/route.ts
@@ -1,10 +1,10 @@
 import { changeStatus } from "@/actions/todo";
 import { NextResponse } from "next/server";
 
-export async function PATCH(req: Request, {params}: {params: {taskId: string}}){
+export async function PATCH(req: Request, { params }: { params: Promise<{ taskId: string }>}){
     try {
         const { isCompleted }: { isCompleted: boolean } = await req.json();
-        const { taskId } = await params;
+        const taskId = (await params).taskId
         // console.log("is com", isCompleted, params.taskId)
       const result = await changeStatus(isCompleted, taskId);
         return NextResponse.json({result}, { status: 200 });
